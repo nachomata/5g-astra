0c744ad4ac268d0455a39a16b57a3eea17087e45
Nacho Mata <nachomataalmagro@gmail.com>
2024-12-16 16:53:12 +0100

    Add frontend structure with initial components, configurations, and styles 🎨

---
+++ frontend/astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+	devToolbar: {
+		enabled: false
+	},
+	output: "server",
+	integrations: [tailwind()]
+});
